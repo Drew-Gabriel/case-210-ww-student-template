2b51c2e287e2095c53a7ab92300f7f4e7fe2a9b4
Drew-Gabriel <126345558+Drew-Gabriel@users.noreply.github.com>
2024-09-28 19:45:44 +0100

    Update Program.cs

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,57 @@
 using System;
 
-class Program
+namespace SimpleFunctionsProgram
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void Main(string[] args)
+        {
+            
+            DisplayWelcome();
+
+            
+            string userName = PromptUserName();
+
+            
+            int userNumber = PromptUserNumber();
+
+            
+            int squaredNumber = SquareNumber(userNumber);
+
+            
+            DisplayResult(userName, squaredNumber);
+        }
+
+        
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        
+        static string PromptUserName()
+        {
+            Console.WriteLine("Please enter your name:");
+            return Console.ReadLine();
+        }
+
+        
+        static int PromptUserNumber()
+        {
+            Console.WriteLine("Please enter your favorite number:");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        
+        static int SquareNumber(int number)
+        {
+            return number * number;
+        }
+
+        
+        static void DisplayResult(string name, int squaredNumber)
+        {
+            Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
+        }
     }
 }
