2fcf1a31553f36e209c2621c8f731b3ab894d8ec
Drew-Gabriel <126345558+Drew-Gabriel@users.noreply.github.com>
2024-09-28 19:38:48 +0100

    Update Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,47 @@
 using System;
 
-class Program
+namespace GradeCalculator
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep2 World!");
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter your grade percentage:");
+            double gradePercentage = Convert.ToDouble(Console.ReadLine());
+
+            
+            char letter;
+            if (gradePercentage >= 90)
+            {
+                letter = 'A';
+            }
+            else if (gradePercentage >= 80)
+            {
+                letter = 'B';
+            }
+            else if (gradePercentage >= 70)
+            {
+                letter = 'C';
+            }
+            else if (gradePercentage >= 60)
+            {
+                letter = 'D';
+            }
+            else
+            {
+                letter = 'F';
+            }
+
+            
+            if (gradePercentage >= 70)
+            {
+                Console.WriteLine($"Congratulations! You passed the course with a grade of {letter}.");
+            }
+            else
+            {
+                Console.WriteLine($"Keep it up! You'll do better next time. Your grade is {letter}.");
+            }
+        }
     }
 }
