dff5d62b27ccf445f66729aa7c2a03c123497065
Drew-Gabriel <126345558+Drew-Gabriel@users.noreply.github.com>
2024-09-28 19:37:17 +0100

    Update Program.cs

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,21 @@
 using System;
 
-class Program
+namespace NameRepeater
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep1 World!");
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("What is your first name?");
+            string firstName = Console.ReadLine();
+
+            
+            Console.WriteLine("What is your last name?");
+            string lastName = Console.ReadLine();
+
+            
+            Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+        }
     }
 }
